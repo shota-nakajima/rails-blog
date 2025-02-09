553378daf5ba9ee4b84020bc442e3e4f7a7df1a9
shota-nakajima <diginarublerebo@yahoo.co.jp>
2025-02-09 18:05:13 +0900

    個別記事への作成者の表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+  validates :user_id, presence: true
 
   def self.search(search)
     if search
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles
+
   has_secure_password
+
   validates :name, presence: true, length: {minimum:2, maximum:32}
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, format: {with: /^[0-9a-zA-Z]+$/, multiline: true}
