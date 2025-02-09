f023b6a3cbef7a1571a7eb2013b50c295157fa04
shota-nakajima <diginarublerebo@yahoo.co.jp>
2025-02-09 10:49:35 +0900

    パスワードをハッシュ化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_05_074409) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_09_014021) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -21,9 +21,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_05_074409) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
