f58b4c04b8d26356f7d5bb5f754d163ae8529620
shota-nakajima <diginarublerebo@yahoo.co.jp>
2025-02-14 16:09:35 +0900

    ユーザーアイコンの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        flash.now[:notice] = "ユーザーの新規登録に成功しました。"
+        flash[:notice] = "ユーザーの新規登録に成功しました。"
         redirect_to root_path
     else
+        Rails.logger.debug("Validation errors: #{@user.errors.full_messages}")  # エラーメッセージをログに出力
         flash.now[:alert] = "ユーザーの新規登録に失敗しました。"
         render :new
     end
@@ -16,6 +17,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 end
